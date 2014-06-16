03546f8ac0a8004ae3399b2315542cbc906b0456
Markus Neubauer <neubauer@email-online.org>
2014-06-16 12:53:33 +0200

    changed db access type

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 logout_redirect
 ===============
 
-Roundcube Webmail Plugin Logout Redirect
+Roundcube Webmail Plugin Logout Redirect depend on domain.
 
 Plugin to redirect on logout to a different website.
 The logout can depend on the domain name (not a must) if you have diffent customers. This way you can redirect to their homepages f.e.
@@ -11,8 +11,12 @@ Download and install via http://plugins.roundcube.net
 Set the following options directly in Roundcube's main config file or via 
 [host-specific](http://trac.roundcube.net/wiki/Howto_Config/Multidomains) configurations:
 
-$config['logout_redirect_url'] = 'http://www.%d/'; // %d ~ users domain part will be take from email address
+```php
+/* %d will be replaced with the default user identity email domain part
+ *    i.e. email@domain.com will replace %d with "domain.com"
+**/
+$config['logout_redirect_url'] = 'http://www.%d/';
+```
 
 Homepage:
 http://www.std-soft.com/index.php/hm-service/81-c-std-service-code/5-rc-plugin-logout-redirect-beim-logout-auf-die-homepage-der-domain-umleiten
-
--- logout_redirect.php
+++ logout_redirect.php
@@ -3,73 +3,70 @@
 /**
  * Redirect on logout
  *
- * @version 1.2 - 2014-06-07
- * @author Roland 'rosali' Liebl
+ * @version 1.4 - 2014-06-07
+ * @Original author Roland 'rosali' Liebl
  * @website http://myroundcube.googlecode.com
  * @licence GNU GPL
  * @history 1.1 - 2010-12-26
- * @        1.2 - 2011-05-04 - rewritten Markus Neubauer: set default return when still %d in return link
+ * @        1.2 - 2011-05-04 - rewritten Markus Neubauer: stripped down to an only logout function with variable return page
  * @        1.3 - 2014-06-07 - adopted for RC 1
+ * @        1.4 - 2014-06-16 - changed db interface to rc 1
  **/
 /**
  * Notice: This plugin must run as the last plugins because it exits
   *         on the 'logout_after' hook !!!
 **/
 /**
- * 1.2 stripped down to an only logout function with variable return page
- * rewritten by Markus Neubauer
- **/
- 
-/**
- *
  * Usage: http://www.std-soft.com/index.php/hm-service/81-c-std-service-code/5-rc-plugin-logout-redirect-beim-logout-auf-die-homepage-der-domain-umleiten
- *
  **/ 
  
 class logout_redirect extends rcube_plugin
 {
-  public $task = 'logout';
+  public $task = 'startup|logout';
+    // we've got no ajax handlers
+    public $noajax = true;
+    // skip frames
+    public $noframe = true;
+
+    private $redirect_url ='';
   
   function init()
   {
-      if ( file_exists("./plugins/logout_redirect/config.inc.php") )
-          $this->load_config('config.inc.php');
-      else
-          $this->load_config('config.inc.php.dist');
-
     $this->add_hook('logout_after', array($this,'logout_after'));
   }
 
   // user logout 
   function logout_after($args)  
   {        
+
     $rcmail = rcmail::get_instance();
     $this->load_config();
 
-    if ( $rcmail->config->get('logout_redirect_url') ) {
+    if ( $this->redirect_url = $rcmail->config->get('logout_redirect_url') ) {
 
-        $redirect = $rcmail->config->get('logout_redirect_url');
-
-        if ( preg_match('/%d/',$redirect) ) {
+        if ( preg_match('/%d/',$this->redirect_url) ) {
+	    // pick users default identity email
             $sql_result = $rcmail->db->query(
-                "SELECT i.email FROM ".$rcmail->config->get('db_table_users')." AS u
-                  JOIN ".$rcmail->config->get('db_table_identities')." AS i 
-                    ON u.user_id = i.user_id where u.username=? and i.standard=1",
+                "SELECT i.email FROM ".$rcmail->db->table_name('users')." AS u
+                  JOIN ".$rcmail->db->table_name('identities')." AS i 
+                    ON u.user_id=i.user_id WHERE u.username=? AND i.standard=1 LIMIT 1;",
               $args['user']);
 
             if ( $sql_result && ($sql_arr = $rcmail->db->fetch_assoc($sql_result)) ) {
                 list($name,$domain) = explode('@', $sql_arr['email']);
-                $redirect = str_replace('%d', $domain, $redirect);
+                $this->redirect_url = str_replace('%d', $domain, $this->redirect_url);
             }
         }
-        if ( !preg_match('/%d/',$redirect) ) {
-            setcookie ('ajax_login','',time()-3600);
-            $rcmail->output->add_script('top.location.href="' . $redirect . '";');
-            $rcmail->output->send('plugin'); 
-            exit; 
-        }
-    }
 
+	// make shure the replacement is done, otherwise continue with normal exit proc
+        if ( !preg_match('/%d/',$this->redirect_url) ) {
+
+            setcookie ('ajax_login','',time()-3600);
+            header("Location: $this->redirect_url", true, 307);
+            exit;
+	}
+
+    }
     return $args; 
   } 
      
